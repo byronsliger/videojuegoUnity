bd574b44238ce160bb9b1f2ba74ff3958d6b9726
BAIRON J ROMERO S <byron.sliger@gmail.com>
2016-10-17 22:50:29 -0500

    Se agrega control Warrior

---
+++ 01_sun_Warrior/New Unity Project/Assets/scripts/ControlWarrior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlWarrior : MonoBehaviour {
+	Rigidbody2D rbd;
+	Animator animator;
+	float malVel = 5f;
+	bool haciaDerecha = true;
+	public float velocity;
+
+	// Use this for initialization
+	void Start () {
+		rbd = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float v = Input.GetAxis ("Horizontal") * malVel;
+		Vector2 vel = new Vector2 (0, rbd.velocity.y);
+		vel.x = v;
+		rbd.velocity = vel;
+		velocity = vel.x;
+
+		animator.SetFloat ("speed", v);
+
+		if (haciaDerecha && v < 0) {
+			haciaDerecha = false;
+			flip ();
+		}else if(!haciaDerecha && v > 0){
+			haciaDerecha = true;
+			flip ();
+		}
+	}
+
+	void flip(){
+		var scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
---
+++ 01_sun_Warrior/New Unity Project/Assets/scripts/ControlZombie.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlZombie : MonoBehaviour {
+	public float vel = -1;
+	Rigidbody2D rbd;
+	Animator animator;
+	static string WALKING = "walking";
+	static string IDLING = "idling";
+	// Use this for initialization
+	void Start () {
+		rbd = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 vector = new Vector2 (vel, 0);
+
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName (WALKING) ) {
+			rbd.velocity = vector;	
+		}
+
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName (WALKING) && Random.value < 1f / (60f * 14f)) {
+			animator.SetTrigger ("idle");
+		} else if (animator.GetCurrentAnimatorStateInfo (0).IsName (IDLING) && Random.value < 1f / (60f * 3f)) {
+			animator.SetTrigger ("walk");
+		}
+
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		flip ();
+	}
+
+	void flip(){
+		vel *= -1;
+		var scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
